02a0284f7c587b4bf31a55b4c110a151bb719529
mahi <mohiddinoshim@yahoo.com>
2020-05-21 22:08:53 +0600

    Variable Mutation and Coercion
    C_09

--- 2-Js-Basic/script.js
+++ 2-Js-Basic/script.js
@@ -1,16 +1,80 @@
 // console.log('Hello World!!!');
 
-var firstName = 'Mahi';
-console.log(firstName);
+/* 
+Variables and data type
+*/
+// var firstName = 'Mahi';
+// console.log(firstName);
 
-var lastName = 'Chowdhury';
-var age = 30;
+// var lastName = 'Chowdhury';
+// var age = 30;
 
-var fullAge = true;
-console.log(fullAge);
+// var fullAge = true;
+// console.log(fullAge);
 
-var job;
-console.log(job);
+// var job;
+// console.log(job);
 
+// job = 'Teacher';
+// console.log(job)
+
+//Variable name rules
+// var _3year = 3;
+// var mahiOshim = 'Mahi and Oshim is a same person';
+// var if = 23;
+
+//Variables mutation and type coercion
+
+
+var firstName = 'John';
+var age = 28;
+
+// Type Coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
 job = 'Teacher';
-console.log(job)
+isMarried = false;
+
+console.log(firstName +' is '+ age +' years old '+ job + ' is he married? '+ isMarried);
+
+// Variable Mutation
+
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName +' is '+ age +' years old '+ job + ' is he married? '+ isMarried);
+
+var lastName = prompt('What is his lastName?');
+console.log(lastName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
