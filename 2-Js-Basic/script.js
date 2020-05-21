ada51faa51aaba58b616aef3c4057cc8c7d8990d
mahi <mohiddinoshim@yahoo.com>
2020-05-21 21:32:53 +0600

    Variable and Data Type

--- 2-Js-Basic/script.js
+++ 2-Js-Basic/script.js
@@ -1,1 +1,16 @@
-console.log('Hello World!!!')
+// console.log('Hello World!!!');
+
+var firstName = 'Mahi';
+console.log(firstName);
+
+var lastName = 'Chowdhury';
+var age = 30;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job)
